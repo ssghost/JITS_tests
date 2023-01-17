876be61fdcb9cba35da942203460e7200d9fa89c
Shuang Song <6882971+ssghost@users.noreply.github.com>
2023-01-17 07:59:40 +0000

    add triton

---
+++ triton-jit.py
@@ -0,0 +1,13 @@
+import triton
+import triton.language as tl
+import random
+
+@triton.jit
+def monte_carlo_pi(ln: int):
+    offset = tl.arange(ln)
+    random_x = tl.rand(offset=offset)
+    random_y = tl.rand(offset=offset)
+    return 4.0 * tl.sum(tl.where((random_x**2 + random_y**2 < 1.0), tl.ones((ln,)), tl.zeros((ln,))))/ ln
+
+if __name__ == "__main__":
+    print(monte_carlo_pi(1000000000))
