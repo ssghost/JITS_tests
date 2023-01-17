49eec852d8b824cdd849f65f1794f882db8cbadb
Shuang Song <6882971+ssghost@users.noreply.github.com>
2023-01-17 06:59:51 +0000

    redesign jax

--- jax-jit.py
+++ jax-jit.py
@@ -2,13 +2,13 @@ import jax
 import jax.numpy as jnp
 
 @jax.jit
-def monte_carlo_pi(random_x, random_y):
+def monte_carlo_pi(ln: int):
+    keyx = jax.random.PRNGKey(111111)
+    keyy = jax.random.PRNGKey(222222) 
+    random_x = jax.random.uniform(keyx, shape=(ln,))
+    random_y = jax.random.uniform(keyy, shape=(ln,))
     return 4.0 * jnp.sum(jnp.where((random_x**2 + random_y**2 < 1.0), jnp.ones((ln,)), jnp.zeros((ln,))))/ ln
 
-ln = 1000000000
-keyx = jax.random.PRNGKey(111111)
-keyy = jax.random.PRNGKey(222222) 
-random_x = jax.random.uniform(keyx, shape=(ln,))
-random_y = jax.random.uniform(keyy, shape=(ln,))
 
-print(monte_carlo_pi(random_x, random_y))
+if __name__ == "__main__":
+    print(monte_carlo_pi(1000000000))
--- tf-jit.py
+++ tf-jit.py
@@ -9,4 +9,5 @@ def monte_carlo_pi(ln: int):
     random_y = tf.random.uniform(shape=(ln,), seed=seedy)
     return 4.0 * tf.sum(tf.where((random_x**2 + random_y**2 < 1.0), np.ones((ln,)), np.zeros((ln,))))/ ln
 
-print(monte_carlo_pi(1000000000))
+if __name__ == "__main__":
+    print(monte_carlo_pi(1000000000))
