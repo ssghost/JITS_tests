f228f5a7c87642099bed9f03e251bebaa4d8419c
Shuang Song <6882971+ssghost@users.noreply.github.com>
2023-01-17 06:52:10 +0000

    add tf

---
+++ tf-jit.py
@@ -0,0 +1,12 @@
+import tensorflow as tf 
+import numpy as np
+
+@tf.function(jit_compile=True)
+def monte_carlo_pi(ln: int):
+    seedx = tf.random.set_seed(111111)
+    seedy = tf.random.set_seed(222222) 
+    random_x = tf.random.uniform(shape=(ln,), seed=seedx)
+    random_y = tf.random.uniform(shape=(ln,), seed=seedy)
+    return 4.0 * tf.sum(tf.where((random_x**2 + random_y**2 < 1.0), np.ones((ln,)), np.zeros((ln,))))/ ln
+
+print(monte_carlo_pi(1000000000))
