311df3a350f78a1a19225ce531cb0310c7ba88e3
ssghost <ssprof0@gmail.com>
2023-01-31 04:05:30 +0800

    add accuracy

--- benchmark_accuracy.py
+++ benchmark_accuracy.py
@@ -7,7 +7,7 @@ def benchmark_accuracy(func: Callable[..., Any]) -> Callable[..., Any]:
     @functools.wraps(func)
     def wrapper(*args: Any, **kwargs: Any) -> Any:
         value = func(*args, **kwargs)
-        accuracy = 1-abs(math.pi - value)/math.pi
+        accuracy = 1.-abs(math.pi - value)/math.pi
         logging.info(f"Execution of {func.__name__} has an accuracy of {accuracy:.2f}.")
         return value
 
